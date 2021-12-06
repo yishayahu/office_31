f6411fc66617c0a9dd2af2156f87d6d8c0ed0ab8
Yishayahu <ishayahu156@gmail.com>
2021-12-06 17:05:29 +0200

    linux support

--- super_cross.py
+++ super_cross.py
@@ -13,12 +13,12 @@ for part_ratio in [0.05, 0.1, 0.2]:
         exp_name = config.split('.')[0]
         config = os.path.join('configs', config)
         if 'source' in exp_name:
-            if part_ratio == '05' and not os.path.exists(os.path.join(paths.out_path, f'{exp_name}\model_final.pth')):
+            if part_ratio == '05' and not os.path.exists(os.path.join(paths.out_path, f'{exp_name}/model_final.pth')):
                 print('running source')
                 os.system(f'python main.py --exp_name {exp_name} --config {config}')
             else:
                 continue
-        if not os.path.exists(os.path.join(paths.out_path, f'{exp_name}_{part_ratio}\model_final.pth')):
+        if not os.path.exists(os.path.join(paths.out_path, f'{exp_name}_{part_ratio}/model_final.pth')):
             print(f'run training {exp_name}_{part_ratio} with config {config}')
             os.system(f'python main.py --exp_name {exp_name}_{part_ratio} --config {config}')
         else:
