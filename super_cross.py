0ab4a24b94959f041cc3cc9ac6234f95b0069c26
Yishayahu <ishayahu156@gmail.com>
2021-12-07 22:47:54 +0200

    all

--- super_cross.py
+++ super_cross.py
@@ -9,10 +9,10 @@ for part_ratio in [0.05, 0.1, 0.2]:
     print('spliting_data')
     split(part_ratio=part_ratio)
     part_ratio = str(part_ratio).split('.')[1]
-    for config in sorted(os.listdir('configs'), key=lambda x: int('source' in x), reverse=True):
+    for config in sorted(os.listdir('configs'), key=lambda x: int('aaaa' in x), reverse=True):
         exp_name = config.split('.')[0]
         config = os.path.join('configs', config)
-        if 'source' in exp_name:
+        if 'aaaa' in exp_name:
             if part_ratio == '05' and not os.path.exists(os.path.join(paths.out_path, f'{exp_name}/model_final.pth')):
                 print('running source')
                 os.system(f'python main.py --exp_name {exp_name} --config {config}')
