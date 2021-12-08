4db9d80d1da1251a1c583b94d2c8a087af591f67
Yishayahu <ishayahu156@gmail.com>
2021-12-08 14:13:48 +0200

    comcom

--- dataset.py
+++ dataset.py
@@ -14,7 +14,9 @@ def get_dataset(dataset_name,split_size, path=paths.data_path):
                 transforms.RandomCrop(224),
                 transforms.RandomHorizontalFlip(),
                 transforms.RandomVerticalFlip(),
+                transforms.RandomRotation(45),
                 transforms.ToTensor(),
+                transforms.ColorJitter(),
                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ]),
             'test': transforms.Compose([
