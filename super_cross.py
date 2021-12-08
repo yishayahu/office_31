eb268858d36f8cc4e4cfebf38ebd0a392fd63b7d
Yishayahu <ishayahu156@gmail.com>
2021-12-08 09:22:06 +0200

    comcom

--- super_cross.py
+++ super_cross.py
@@ -1,7 +1,7 @@
 import os
 
 import paths
-from main import main, fix_seed
+from main import fix_seed
 from split_dataset_folder import split
 
 fix_seed()
@@ -9,10 +9,16 @@ for part_ratio in [0.05, 0.1, 0.2]:
     print('spliting_data')
     split(part_ratio=part_ratio)
     part_ratio = str(part_ratio).split('.')[1]
-    for config in sorted(os.listdir('configs'), key=lambda x: int('aaaa' in x), reverse=True):
+    exps = ['webcam_source.yml', 'webcam_target.yml',
+            'webcam_target_base.yml', 'webcam_target_continue_optimizer.yml',
+            'webcam_target_continue_optimizer_09.yml', 'webcam_target_keep_source.yml',
+            'amazon_source.yml', 'amazon_target.yml', 'amazon_target_base.yml',
+            'amazon_target_continue_optimizer.yml', 'amazon_target_continue_optimizer_09.yml',
+            'amazon_target_keep_source.yml']
+    for config in exps:
         exp_name = config.split('.')[0]
         config = os.path.join('configs', config)
-        if 'aaaa' in exp_name:
+        if exp_name == 'webcam_source' or exp_name == 'amazon_source':
             if part_ratio == '05' and not os.path.exists(os.path.join(paths.out_path, f'{exp_name}/model_final.pth')):
                 print('running source')
                 os.system(f'python main.py --exp_name {exp_name} --config {config}')
