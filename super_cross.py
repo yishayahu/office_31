627e562c77c67f009f1c011d893fa34bc0cc541b
Yishayahu <ishayahu156@gmail.com>
2021-12-14 08:00:13 +0200

    cccc

--- super_cross.py
+++ super_cross.py
@@ -13,7 +13,8 @@ pp = []
 
 exps = ['webcam_source.yml','amazon_source.yml', 'webcam_target.yml',
         'webcam_target_base.yml', 'webcam_target_combined.yml',
-        'webcam_target_continue_optimizer_09.yml', 'webcam_target_keep_source.yml'
+        'webcam_target_continue_optimizer_09.yml', 'webcam_target_keep_source.yml',
+        'webcam_target_combined_keep_source.yml'
         , 'amazon_target.yml', 'amazon_target_base.yml',
          'amazon_target_continue_optimizer_09.yml','amazon_target_combined.yml',
         'amazon_target_keep_source.yml','amazon_target_combined_keep_source.yml']
