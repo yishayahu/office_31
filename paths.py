1522f17433c9dd00f5ec72f88e8002e591237751
Yishayahu <ishayahu156@gmail.com>
2021-12-21 21:51:31 +0200

    paper

--- paths.py
+++ paths.py
@@ -3,8 +3,8 @@ import sys
 if sys.platform == 'win32':
     data_path = r'C:\Users\Y\PycharmProjects\domain_adaptation_images'
     pretrained_models_path = r'C:\Users\Y\PycharmProjects\domain_adaptation_out'
-    out_path = r'C:\Users\Y\PycharmProjects\domain_adaptation_out'
+    out_path = r'C:\Users\Y\PycharmProjects\domain_adaptation_out2'
 else:
     data_path ='/home/dsi/shaya/domain_adaptation_images2/'
     pretrained_models_path = '/home/dsi/shaya/domain_adaptation_images2/outs/'
-    out_path ='/home/dsi/shaya/domain_adaptation_images2/outs/'
+    out_path ='/home/dsi/shaya/domain_adaptation_images2/outs2/'
